cdbf06472b4627909fdee0614e4e047dc3ee0e66
Pedro Moura <pedrohenriquedelgado@ice.ufjf.br>
2022-04-26 22:57:39 -0300

    .

--- src/main/java/br/ufjf/dcc193/pedromoura/quest/Clerigo.java
+++ src/main/java/br/ufjf/dcc193/pedromoura/quest/Clerigo.java
@@ -8,7 +8,7 @@ public class Clerigo implements Heroi{
     public Clerigo(){
         System.out.println("Um novo clerigo chega ao reino...");
     }
-
+ 
     public void agir(){
         System.out.println("O clerigo luta!");
     }
